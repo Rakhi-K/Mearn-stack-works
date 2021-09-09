792e4210cad6a2e5daca4c5b53b69846b75cd634
Rakhi <rakhikarayi96@gmail.com>
2021-09-09 11:36:32 +0530

    file

---
+++ JAVASCRIPTS/script.js
@@ -0,0 +1,63 @@
+// Java script coding
+
+
+//To print a msg in console
+//print() - used in python
+//printf()- used in c
+//cout<<    -used in c++
+//System.out.Println() - in java
+//console.log()  - in javascript
+
+
+console.log("hello world");
+
+//---------identifiers ---------//   
+
+//variable name- in JS lowercase, noun, emp_name
+
+
+//function name-  displayvslues => displayValue
+//print person() -snack notation - in pyhton used most
+//printPerson()  -camel case in java , javascript
+
+//class Person{}  => Person => classname PersonDetails()
+
+
+//variables - 
+
+    //used to represent a memory location,
+    
+// variables created in javascript =>
+       //var, let, const    -- globally used - var
+       
+    //    //pi-3.14 eg:
+    //    const pi=3.14;
+    //    pi=3.25;
+    //    console.log(pi);
+
+
+var company="luminar"
+var loc_name="abc"
+console.log(company);
+console.log(loc_name);
+
+
+
+var emp_name="Rakhi"
+var age="24years"
+var content="I am Rakhi. 24 years"
+console.log(emp_name);
+console.log(age);
+console.log(content);
+
+
+
+
+
+
+
+
+
+
+
+
